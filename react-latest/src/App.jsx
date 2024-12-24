f9168b16947bffef71e54990e7b977a53af45af0
ibm.i.saini <anisain@apac.corpdir.net>
2024-12-24 12:42:59 +0530

    JSX and create element by react

--- react-latest/src/App.jsx
+++ react-latest/src/App.jsx
@@ -1,14 +1,17 @@
-import Login, {Profile, Setting} from "./UserComponent.jsx";
+import { createElement } from "react";
+// import Login, {Profile, Setting} from "./UserComponent.jsx";
 
 function App(){
-  return (
-    <div>
-      <h1>React Import - Export component</h1>
-      <Login />
-      <Profile />
-      <Setting />
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <h1>React Import - Export component</h1>
+  //     <Login />
+  //     <Profile />
+  //     <Setting />
+  //   </div>
+  // )
+
+  return createElement("h1",{id:"customID", class: "customClass"}, "Creating HTML element by react only")
 }
 
 export default App;
