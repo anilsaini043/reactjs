0abe0011729466c4467906105c02767a135fc689
ibm.i.saini <anisain@apac.corpdir.net>
2024-12-24 12:54:41 +0530

    More about JSX

--- react-latest/src/App.jsx
+++ react-latest/src/App.jsx
@@ -1,17 +1,30 @@
-import { createElement } from "react";
-// import Login, {Profile, Setting} from "./UserComponent.jsx";
+// import { createElement } from "react";
+import Login, {Profile, Setting} from "./UserComponent.jsx";
 
 function App(){
-  // return (
-  //   <div>
-  //     <h1>React Import - Export component</h1>
-  //     <Login />
-  //     <Profile />
-  //     <Setting />
-  //   </div>
-  // )
+  const name = "Anil kumar saini";
 
-  return createElement("h1",{id:"customID", class: "customClass"}, "Creating HTML element by react only")
+  function sum(){
+    return 10+20
+  }
+
+  function multiply(a,b){
+    return a*b
+  }
+
+  return (
+    <div>
+      <h1>React Import - Export component</h1>
+      <h1>My name is : {name}</h1>
+      <h2>Sum of two no is : {sum()}</h2>
+      <h2>Multiply of two no is : {multiply(5,7)}</h2>
+      <Login />
+      <Profile />
+      <Setting />
+    </div>
+  )
+
+  // return createElement("h1",{id:"customID", class: "customClass"}, "Creating HTML element by react only")
 }
 
 export default App;
