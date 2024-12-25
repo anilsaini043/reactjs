09737bd2db28bd1e1c3135692cea6a370d60a57e
ibm.i.saini <anisain@apac.corpdir.net>
2024-12-25 14:33:24 +0530

    Show hide or toggle feature

--- react-latest/src/App.jsx
+++ react-latest/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import Counter from "./Counter.jsx"
+import Counter from "./Counter.jsx";
 
 function App(){
   const [ name, setName ] = useState("Anil Kumar");
+  const [show, setShow] =  useState(true);
+
   const changeName = () => {
     setName("Anil Kumar Saini")
   }
+
   return (
     <div>
       <h1>My name is : {name}</h1>
       <button onClick={changeName}>Change Name</button>
       <Counter />
+
+      <h2>Show hide feature</h2>
+      {
+        show ? <h3>Toggle me</h3> : null
+      }
+
+      <button onClick={()=> setShow(!show)}>Click to toggle</button>
     </div>
   )
  
